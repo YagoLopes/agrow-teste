949c3a3700bbe87a4d1e7534cd9a91c82e81bd4c
Yago Lopes <yagolopeslazaro@gmail.com>
2019-11-02 16:59:17 -0300

    refactore

--- frontend/src/pages/DemandList/styles.js
+++ frontend/src/pages/DemandList/styles.js
@@ -19,16 +19,7 @@ export const Container = styled.div`
 
 
 
-@keyframes fade {
-    from {
-        opacity: 0;
-        transform: scale(0.9);
-    }
-    to {
-        opacity: 1;
-        transform: scale(1);
-    }
-}
+
 
 
     article {
--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -35,7 +35,16 @@ export default createGlobalStyle`
       cursor: pointer;
     }
 
-  
+    @keyframes fade {
+        from {
+            opacity: 0;
+            transform: scale(0.9);
+        }
+        to {
+            opacity: 1;
+            transform: scale(1);
+        }
+    }
 
 
     
