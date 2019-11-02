d30de99c86a884b5eca296559047c7b97bd2a43f
Yago Lopes <yagolopeslazaro@gmail.com>
2019-11-02 18:05:21 -0300

    removendo dependencias quebradas

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Nav from "../../components/nav";
 
 import { Container } from './styles';
+import Barra from "../../components/graphics";
 
 export default function Main() {
   return (
     <>
     <Nav/>
-<Container/>
+<Container>
+<article>
+<Barra/>
+</article>
+  </Container>
     </>
   );
 }
--- frontend/src/pages/Main/styles.js
+++ frontend/src/pages/Main/styles.js
@@ -1,5 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  
-  `;
+  width: 100%;
+  height: 100%;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+    article {
+    background-color: #fff;
+    max-width: 90%;
+    min-width: 50%;
+    border-radius: 5px;
+    display:flex;
+    flex-direction:column;
+    border: 1px solid #ddd;
+    padding: 20px;
+    margin: 20px 0 0 20px;
+
+  }
+`;
